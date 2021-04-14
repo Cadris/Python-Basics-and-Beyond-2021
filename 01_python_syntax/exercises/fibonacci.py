ec58ba529aff4e8f610de4a2b9c5ceab5a84a65a
Sayan Dasgupta <cadrissilver@gmail.com>
2021-04-14 15:25:13 +0530

    Fibonachi Done And Obsidian Vault Added

--- 01_python_syntax/exercises/fibonacci.py
+++ 01_python_syntax/exercises/fibonacci.py
@@ -4,13 +4,45 @@
     0,1,1,2,3,5...
 """
 
-def fib(n):
+def fibo(n):
 
     a = 0 
     b = 1
-    fibo = 0
-    # TODO write fibonacci code
+    c = 0
 
+    for i in range(1,n+1):
+        print(c ,end=", ")
+        c=a+b
+        if i==2:
+            c=1
+        a,b=b,c
 
-# print the first 100 fibonacci numbers
-fib(100)
+# def fiboR(n,a=0,b=1,c=0):
+#     if n<=1:
+#         print(c)
+#         return
+#     else:
+#         print(c, end=", ")
+#         c=a+b
+#         a,b=b,c
+#         if(a==1 & b==1):
+#             c=1
+#         fiboR(n-1,a,b,c)
+#         return
+# Python program to display the Fibonacci sequence
+
+def fiboR(n):
+   if n <= 1:
+       print(n, end="")
+       return n
+   else:
+       d1 = fiboR(n-1)
+       d2 = fiboR(n-2)
+       print((d1+d2), end=", ")
+       return(d1+d2)
+
+# Driver Program
+limit = 5
+fibo(limit)
+print("\n=====================")
+fiboR(limit)
